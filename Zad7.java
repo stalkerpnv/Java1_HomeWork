dfad46cdc5dc1a1269751d259166972ac4773418
stalkerpnv <45097338+stalkerpnv@users.noreply.github.com>
2018-11-23 21:45:37 +0900

    Delete Zad7.java

--- Zad7.java
+++
@@ -1,8 +0,0 @@
-public class Zad7 {
-    public static void sayHello(String name){
-        System.out.println("Привет,"+name+"!");
-    }
-    public static void main(String[]args){
-        sayHello("John");
-    }
-}
